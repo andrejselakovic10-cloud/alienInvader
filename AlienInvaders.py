ff03236656d0160e88110825fef8eb17a9cceb4c
Andrej Selakovic <andrejselakovic10@gmail.com>
2026-07-20 22:15:34 +0200

    movment

--- AlienInvaders.py
+++ AlienInvaders.py
@@ -10,22 +10,34 @@ class AlienInvaders:
     
     def __init__(self):
         pygame.init()
-
-        self.settings = Settings() #KOMPOZICIJA
-        self.screen = self.settings.screen
-        self.ship = Ship(self.screen)
-        print(dir(self.ship))
+        
+        self.__settings = Settings() 
+        self.__screen = self.__settings.screen
+        self.__ship = Ship(self.__screen)
+    def __eventThings(self):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()  
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.__ship.right = True
+                    if event.key == pygame.KEYUP:
+                        self.__ship.right = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.__ship.left = True
+                    if event.key == pygame.KEYUP:
+                        self.__ship.left = False
+    def __screenThings(self):
+        self.__screen.fill(self.__settings.bg_color)
+        self.__ship.drawShip()
+        pygame.display.flip()
+        
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if(event.type == pygame.QUIT):
-                    sys.exit()       
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.drawShip()
-            pygame.display.flip()
-   
-
+            self.__eventThings()
+            self.__screenThings()
+    
 if __name__ == "__main__":
     ai = AlienInvaders() 
     ai.run_game()
--- Ship.py
+++ Ship.py
@@ -4,8 +4,22 @@ class Ship:
     def __init__(self,screen):
         self.screen = screen
         self.ship = pygame.image.load("assets/ship.png")
-        self.ship = pygame.transform.scale(self.ship,(200, 200))
+        self.shipSetup()
+        self.left = False
+        self.right = False
+    def shipSetup(self):
+    #sizeeee
+        scale = 0.25
+        shipHeight = self.ship.get_height() * scale
+        shipWidth = self.ship.get_width() * scale
+        self.ship = pygame.transform.scale(self.ship,(shipWidth,shipHeight))
+    #postitonnnnnnnnn
         self.rect = self.ship.get_rect()
+        self.rect.midbottom = self.screen.get_rect().midbottom
+
     def drawShip(self):
         self.screen.blit(self.ship, self.rect)
-        
+    def right(self):
+        self.rect.x += 15
+    def left(self):
+        self.rect.x -= 15
